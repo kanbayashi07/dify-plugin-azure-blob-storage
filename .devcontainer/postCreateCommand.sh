04db9bf3f230b929ab90ef0a03cb5bb8acd08444
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2025-01-10 16:54:17 +0000

    Remove outdated Dify tools script and streamline installation in postCreateCommand

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-/bin/bash .devcontainer/download_dify_tools.sh
+# Install Dify Plugin CLI
+curl -s https://api.github.com/repos/langgenius/dify-plugin-daemon/releases/latest \
+  | jq -r ".assets[] | select(.name | test(\"dify-plugin-linux-amd64\")) | .browser_download_url" \
+  | xargs -n 1 sudo curl -L -o /usr/local/bin/dify
+sudo chmod +x /usr/local/bin/dify
+
+# Install python packages
 if [ -f "requirements.txt" ]; then
   pip3 install --user -r requirements.txt
 fi
+
+# Avoid to create __pycache__ directory
+echo 'export PYTHONDONTWRITEBYTECODE=1' >> ~/.bashrc
